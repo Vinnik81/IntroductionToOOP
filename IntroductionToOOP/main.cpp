3edf0f438fe2d3c9d362ce461afa9cd21935c8c8
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2021-12-18 22:27:23 +0600

    Operators_Overloding

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+using std::cout;
 #define tab "\t"
 
 class Point
@@ -24,10 +24,97 @@ public:
 	{
 		this->y = y;
 	}
-}A,B,C;
+	                  //     Consructor:
+	/*Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor: " << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	};
+	        //     Operators:
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	Point& operator+=(const Point& other)
+	{
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
+	}
 
+	Point& operator++()
+	{
+		x++;
+		y++;
+		return *this;
+	}
+
+	Point operator++(int)
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
+
+	Point& operator()(double x, double y)
+	{
+		set_x(x);
+		set_y(y);
+		return *this;
+	}
+
+	      //     Methods:
+	double distance(const Point& other) const
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
+};
+double distance(const Point& A, const Point& B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(pow(x_distance, 2) + pow(y_distance, 2));
+}
+
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+
+//#define CONSTRUCTORS_CHECK
 //#define StRUCT_POINT
-
+//#define DISTANCE_CHECK
+//#define ASSIGNMENT_CHEK
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -42,9 +129,66 @@ void main()
 	cout << pA->x << tab << pA->y << endl;
 #endif //  STRUCT_POINT
 
+#ifdef CONSTRUCTORS_CHECK
 	Point A;
-	A.set_x(2);
-	A.set_y(3);
-	cout << A.get_x() << tab << A.get_y() << endl;
+	double x, y;
+	//A.set_x(2);
+	//A.set_y(3);
+	//cout << A.get_x() << tab << A.get_y() << endl;
+	A.print();
 
+	Point B(22, 33);
+	B.print();
+
+	Point C = 5;
+	C.print();
+
+	Point D(0, 123);
+	D.print();
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef DISTANCE_CHECK
+	Point A(2, 3);
+	Point B(7, 8);
+	cout << "----------------------------------------------------------------\n";
+	cout << "Расстояние от точки 'А' до точки 'В': " << A.distance(B) << endl;
+	cout << "----------------------------------------------------------------\n";
+	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+	cout << "----------------------------------------------------------------\n";
+	cout << "Расстояние между 'A' и 'B': " << distance(A, B) << endl;
+	cout << "----------------------------------------------------------------\n";
+	cout << "Расстояние между 'B' и 'A': " << distance(B, A) << endl;
+	cout << "----------------------------------------------------------------\n";
+#endif // DISTANCE_CHECK
+	//Point A(2, 3);
+	//Point B = A; // CopyConstructor
+	//B.print();
+	//Point C; //DefaultConstructor
+	//C = B;
+	//C.print();
+#ifdef ASSIGNMENT_CHEK
+	Point A, B, C;
+	cout << "\n--------------------------------\n";
+	A = B = C = Point(7, 8);
+	cout << "\n--------------------------------\n";
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHEK
+
+	Point A(2, 3);
+	Point B(7, 8);
+	/*Point C = A + B;
+	C.print();
+	A += B;
+	A.print();
+	C++;
+	C.print();*/
+	A = B++;
+	A.print();
+	B.print();
+	/*B.set_x(123);
+	B.set_y(234);*/
+	B(123, 234);
+	B.print();
 }
